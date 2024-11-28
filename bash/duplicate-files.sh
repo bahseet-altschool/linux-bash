a5227128168d887a8c51aed60975fe87deac2940
Basit Adebayo <bahseet2022@gmail.com>
2024-11-28 11:04:14 +0100

    I input description of the commands for better understanding

--- bash/duplicate-files.sh
+++ bash/duplicate-files.sh
@@ -3,6 +3,7 @@
 #Develop a script to find and list duplicate files in a directory based on file size or checksum.
 
 
+#To check if argument is provided
 
 if [[ -z "$1" ]]; then
 echo "Usage: $0 <directory_path>"
@@ -11,16 +12,21 @@ fi
 
 directory="$1"
 
+
+#To check that the directory path is valid
 if [[ ! -d "$directory" ]]; then
 echo "Directory $directory not found"
 exit 1
 fi
 
+
+#Find files in the directory
 declare -A sizes
 while IFS=' ' read -r size name; do
 sizes["$size"]+="$name "
 done < <(find "$directory" -type f -exec stat --format='%s %n' {} + | sort -n)
 
+#Displaying/listing of duplicate files
 for size in "${!sizes[@]}"; do
 files=(${sizes["$size"]})
 if [[ ${#files[@]} -gt 1 ]]; then
